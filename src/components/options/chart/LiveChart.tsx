f1cc4076c2f34ee7255bb6ea011120e07a6903ad
shehan <shehan8998@gmail.com>
2026-08-22 12:37:28 +0530

    Fix Stochastic: use smoothing=3 to match Deriv Y,Y (Slow Stochastic) with Close/Close mode

--- src/components/options/chart/LiveChart.tsx
+++ src/components/options/chart/LiveChart.tsx
@@ -1035,8 +1035,8 @@ function syncIndicators(
 
         const pK = ind.params.period || 14;
         const pD = 3;
-        // Temporary test: force smoothing to 1 to see if Deriv plots Fast Stochastic
-        const smoothing = 1;
+        // Deriv uses Slow Stochastic (Y,Y) = 3-period smoothing on K, 3-period on D
+        const smoothing = 3;
         
         let targetArray = valueArray;
         switch (ind.params.field) {
